72c2aeebf74a0bb1d2558699af300ced1433b4c1
EricHodgins <hodgins.e@gmail.com>
2020-07-31 14:20:16 -0400

    basic monster card layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,22 +7,22 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Eric'
-    }
+      monsters: []
+    };
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({ string: 'Hello?'})}>
-            Change Text
-          </button>
-        </header>
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {    
+    return (      
+      <div className='App'>
+        <CardList monsters={this.state.monsters} />
       </div>
-    );
+    );    
   }
 }
 
